29cc6b89b25e4596bf5824fc513b494f31cac305
Simone823 <simone.daglio823@outlook.it>
2022-04-26 18:33:45 +0200

    route homepage

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+// Route homepage
+Route::get('/', function () {
+    return view('layouts.default');
+})->name('homepage');
 
+// Route comic controller
 Route::resource('comics', 'ComicController');
