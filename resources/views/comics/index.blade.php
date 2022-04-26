f351469232d154d452048c9ae4d1c0ecacf8f6cd
Simone823 <simone.daglio823@outlook.it>
2022-04-26 18:11:01 +0200

    default layout, comics.index component

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.default')
+@section('headeTilte', 'Comics')
+
+@section('mainContent')
+
+    {{-- Table comics --}}
+    <table>
+
+        {{-- Table header --}}
+        <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>price</th>
+            <th>series</th>
+            <th>sale_date</th>
+            <th>type</th>
+        </tr>
+
+            @foreach ($comics as $element)
+                <tr>
+                    <td>
+                        {{$element['id']}}
+                    </td>
+
+                    <td>
+                        {{$element['title']}}
+                    </td>
+
+                    <td>
+                        {{$element['price']}}
+                    </td>
+
+                    <td>
+                        {{$element['series']}}
+                    </td>
+
+                    <td>
+                        {{$element['sale_date']}}
+                    </td>
+
+                    <td>
+                        {{$element['type']}}
+                    </td>
+
+                    <td>
+                        <a href="{{route('comics.show', $element['id'])}}">Visualizza</a>
+                    </td>
+                </tr>
+            @endforeach
+
+    </table>
+
+@endsection
