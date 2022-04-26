1a01fd5b80dbdc5008467ab7256a6ca18ba4e3b9
Simone823 <simone.daglio823@outlook.it>
2022-04-26 21:47:23 +0200

    header scss, component header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,14 @@
+{{-- Header --}}
+<header>
+    <ul class="menu_link">
+        <li>
+            <a href="{{route('homepage')}}">Homepage</a>
+        </li>
+        <li>
+            <a href="{{route('comics.index')}}">Comics</a>
+        </li>
+        <li>
+            <a href="{{route('comics.create')}}">Aggiungi nuovo comic</a>
+        </li>
+    </ul>
+</header>
