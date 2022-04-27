a386fcf7d0f2d3236e351ad3a0132e7f936c5bdb
Simone823 <simone.daglio823@outlook.it>
2022-04-27 16:36:59 +0200

    edit component comics create form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,27 +5,28 @@
 @section('mainContent')
 
     {{-- Form  --}}
-    <form action="" method="">
+    <form action="{{route('comics.store')}}" method="POST">
+        @csrf
 
         {{-- Titile --}}
         <label for="title" id="title" name="title">Titolo</label>
-        <input type="text" id="title" name="title">
+        <input type="text" id="title" name="title" placeholder="Inserisci il titolo">
 
         {{-- Description --}}
         <label for="description" id="description" name="description">Descrizione</label>
-        <input type="text"id="description"  name="description">
+        <textarea name="description" id="description" cols="30" rows="10" placeholder="Inserisci una descrizione" resize></textarea>
 
         {{-- Thumb --}}
         <label for="thumb" id="thumb" name="thumb">Immagine</label>
-        <input type="text" id="thumb" name="thumb">
+        <input type="text" id="thumb" name="thumb" placeholder="Url Immagine">
         
         {{-- Price --}}
         <label for="price" id="price" name="price">Prezzo</label>
-        <input type="text" id="price" name="price">
+        <input type="text" id="price" name="price" value="$">
 
         {{-- Series --}}
         <label for="series" id="series" name="series">Collezione</label>
-        <input type="text" id="series" name="series">
+        <input type="text" id="series" name="series" placeholder="Nome collezione">
 
         {{-- Sale date --}}
         <label for="sale_date" id="sale_date" name="sale_date">Data di vendita</label>
@@ -33,7 +34,11 @@
 
         {{-- Type --}}
         <label for="type" id="type" name="type">Tipologia</label>
-        <input type="text" id="type" name="type">
+        <select name="type" id="type">
+            <option value="" hidden>Seleziona una tipologia</option>
+            <option value="comic book">comic book</option>
+            <option value="graphic novel">graphic novel</option>
+        </select>
 
         {{-- Submit --}}
         <button type="submit">Aggiungi</button>
